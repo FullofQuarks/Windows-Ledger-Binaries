4225e764bdd0e90717c80a1f8a29850afa24368e
Nicholas Smith <nsmith@ethosgroup.com>
2023-04-04 15:32:53 -0500

    Added note about copying mparam header

--- build.ps1
+++ build.ps1
@@ -7,6 +7,7 @@ cd boost
 cd ..\mpir\msvc\vs22
 .\msbuild.bat gc LIB x64 Release
 cd ..\..\..\mpfr\build.vs22\lib_mpfr
+# copy \mpfr\src\x86_64\core2 --> \mpfr\src\x86_64\corei5\
 msbuild /p:Configuration=Release /p:Platform=x64 lib_mpfr.vcxproj
 cd ..\..\..\ledger
 cmake -DCMAKE_BUILD_TYPE:STRING="Release" -DUSE_PYTHON=OFF -DBUILD_LIBRARY=OFF -DMPFR_LIB:FILEPATH="../../mpfr/build.vs22/lib/x64/Release/mpfr" -DGMP_LIB:FILEPATH="../../mpir/lib/x64/Release/mpir" -DMPFR_PATH:PATH="../mpfr/lib/x64/Release" -DGMP_PATH:PATH="../mpir/lib/x64/Release" -DBUILD_DOCS:BOOL="0" -DHAVE_REALPATH:BOOL="0" -DHAVE_GETPWUID:BOOL="0" -DHAVE_GETPWNAM:BOOL="0" -DHAVE_IOCTL:BOOL="0" -DHAVE_ISATTY:BOOL="0" -DBOOST_ROOT:PATH="../boost/" -DBoost_USE_STATIC_LIBS:BOOL="1" -DCMAKE_CXX_FLAGS_RELEASE:STRING="/MT /Zi /Ob0 /Od" -A x64 -G "Visual Studio 17"

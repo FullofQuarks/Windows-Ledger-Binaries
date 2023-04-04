66d51f9b259f633d00a0c9cd5b4cf3fc959e207b
Nicholas Smith <nsmith@ethosgroup.com>
2023-04-04 14:20:22 -0500

    updated build script for vs22 and x64

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@
 # git submodule foreach --recursive git clean -x -f -d
 cd boost
 .\bootstrap.bat
-.\b2.exe link=static runtime-link=static threading=multi --layout=versioned
-cd ..\mpir\msvc\vs19
-.\msbuild.bat gc LIB Win32 Release
-cd ..\..\..\mpfr\build.vs19\lib_mpfr
-msbuild /p:Configuration=Release lib_mpfr.vcxproj
+.\b2.exe link=static runtime-link=static threading=multi address-model=64 -a --layout=versioned
+cd ..\mpir\msvc\vs22
+.\msbuild.bat gc LIB x64 Release
+cd ..\..\..\mpfr\build.vs22\lib_mpfr
+msbuild /p:Configuration=Release /p:Platform=x64 lib_mpfr.vcxproj
 cd ..\..\..\ledger
-cmake -DCMAKE_BUILD_TYPE:STRING="Release" -DBUILD_LIBRARY=OFF -DMPFR_LIB:FILEPATH="../../mpfr/build.vs19/lib/Win32/Release/mpfr" -DGMP_LIB:FILEPATH="../../mpir/lib/win32/Release/mpir" -DMPFR_PATH:PATH="../mpfr/lib/Win32/Release" -DGMP_PATH:PATH="../mpir/lib/win32/Release" -DBUILD_DOCS:BOOL="0" -DHAVE_REALPATH:BOOL="0" -DHAVE_GETPWUID:BOOL="0" -DHAVE_GETPWNAM:BOOL="0" -DHAVE_IOCTL:BOOL="0" -DHAVE_ISATTY:BOOL="0" -DBOOST_ROOT:PATH="../boost/" -DBoost_USE_STATIC_LIBS:BOOL="1" -DCMAKE_CXX_FLAGS_RELEASE:STRING="/MT /Zi /Ob0 /Od" -A Win32 -G "Visual Studio 16"
-msbuild /p:Configuration=Release src\ledger.vcxproj
+cmake -DCMAKE_BUILD_TYPE:STRING="Release" -DUSE_PYTHON=OFF -DBUILD_LIBRARY=OFF -DMPFR_LIB:FILEPATH="../../mpfr/build.vs22/lib/x64/Release/mpfr" -DGMP_LIB:FILEPATH="../../mpir/lib/x64/Release/mpir" -DMPFR_PATH:PATH="../mpfr/lib/x64/Release" -DGMP_PATH:PATH="../mpir/lib/x64/Release" -DBUILD_DOCS:BOOL="0" -DHAVE_REALPATH:BOOL="0" -DHAVE_GETPWUID:BOOL="0" -DHAVE_GETPWNAM:BOOL="0" -DHAVE_IOCTL:BOOL="0" -DHAVE_ISATTY:BOOL="0" -DBOOST_ROOT:PATH="../boost/" -DBoost_USE_STATIC_LIBS:BOOL="1" -DCMAKE_CXX_FLAGS_RELEASE:STRING="/MT /Zi /Ob0 /Od" -A x64 -G "Visual Studio 17"
+msbuild /p:Configuration=Release /p:Platform=x64 src\ledger.vcxproj
 copy Release\ledger.exe ..\
